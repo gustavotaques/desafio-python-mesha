bd0ed69fad174ed6c313e6dbc81d98933232fa58
Gustavo Taques <gustavotaques@hotmail.com>
2021-10-13 01:33:43 -0300

    alterações pelo notebook

--- LSM/models.py
+++ LSM/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils.timezone import localdate
 
 class Obra(models.Model):
     titulo = models.CharField(max_length=50)
     editora = models.CharField(max_length=30)
     foto = models.URLField()
     autores = models.JSONField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=localdate)
 
     def __str__(self) -> str:
         return self.titulo
--- LSM/views.py
+++ LSM/views.py
@@ -4,20 +4,52 @@ from .serializers import ObraSerializer
 from rest_framework import response, viewsets
 from rest_framework.views import APIView
 import csv
+import xlwt
 
 class ObraViewSet(viewsets.ModelViewSet):
     queryset = Obra.objects.all()
     serializer_class = ObraSerializer
 
 
-class ExportCSVObras(APIView):
-    def get(self, request, *args, **kwargs):
-        obras = Obra.objects.all()
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="export-obras.csv"'
-        writer = csv.writer(response)
+# class ExportCSVObras(APIView):
+#     def get(self, request):
+
+#         response = HttpResponse(content_type='text/csv')
+#         response['Content-Disposition'] = 'attachment; filename="export-obras.csv"'
+#         writer = csv.writer(response)
         
-        for obra in obras:
-            print(obra)
+#         writer.writerow(['id', 'titulo', 'editora', 'foto', 'autores', 'created_at'])
+
+#         for data in Obra.objects.all().values_list('id', 'titulo', 'editora', 'foto', 'autores', 'created_at'):
+#             print(data)
+#             writer.writerow(data)
+
+#         return response
+
+
+class ExportCSVObras(APIView):
+    def get(self, request):
+
+        response = HttpResponse(content_type='text/csms-excel')
+        response['Content-Disposition'] = 'attachment; filename="export-obras.xls"'
+        
+        wb = xlwt.Workbook(encoding='utf-8')
+        ws = wb.add_sheet("Obras")
+        row_num = 0
+
+        colums = ['id', 'titulo', 'editora', 'foto', 'autores', 'created_at']
+
+        for col_num in range(len(colums)):
+            ws.write(row_num, col_num, colums[col_num])
+        
+        rows = Obra.objects.all().values_list('id', 'titulo', 'editora', 'foto', 'autores', 'created_at')
+
+        for row in rows:
+            row_num += 1
+
+            for col_num in range(len(row)):
+                ws.write(row_num, col_num, str(row[col_num]))
+        
+        wb.save(response)
 
         return response
